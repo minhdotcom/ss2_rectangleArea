5d9f4a0e4bf25ee4a23b5058f293afa43b1810ab
Minh <minhdotcom@gmail.com>
2019-05-06 17:04:18 +0700

    done

--- src/SystemTime.java
+++ src/SystemTime.java
@@ -1,8 +1,14 @@
-import java.util.Date;
+import java.util.Scanner;
 
 public class SystemTime {
     public static void main(String[] args) {
-        Date now = new Date();
-        System.out.println("Now is " + now);
+        float width, height;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter width: ");
+        width = scanner.nextFloat();
+        System.out.println("Enter height: ");
+        height = scanner.nextFloat();
+        float area = width * height;
+        System.out.println("Area is: " + area);
     }
 }
